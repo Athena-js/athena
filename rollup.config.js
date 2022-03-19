51e9d04e19edcf5c761650c4549b28e3c0a177c3
Vincent0700 <wang.yuanqiu007@gmail.com>
2022-03-19 12:50:58 +0800

    build: use ttypescript plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,20 @@
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import ttypescript from 'ttypescript';
+import tsPlugin from 'rollup-plugin-typescript2';
+import cleaner from 'rollup-plugin-cleaner';
 import commonjs from '@rollup/plugin-commonjs';
-import alias from '@rollup/plugin-alias';
 import serve from 'rollup-plugin-serve';
 import { string } from 'rollup-plugin-string';
 import { terser } from 'rollup-plugin-terser';
-import * as path from 'path';
 
 const isProd = process.env.NODE_ENV === 'production';
-const projectRootDir = path.resolve(__dirname);
-
-const resolver = resolve({
-  extensions: ['.js', '.wgsl']
-});
 
 const commonPlugins = [
   resolve(),
   commonjs(),
-  typescript(),
-  alias({
-    entries: [{ find: '@', replacement: path.resolve(projectRootDir, 'src') }],
-    resolver
-  }),
-  string({
-    include: '**/*.wgsl'
-  })
+  tsPlugin({ typescript: ttypescript }),
+  cleaner({ targets: ['dist'] }),
+  string({ include: '**/*.wgsl' })
 ];
 
 const devPlugins = () => [
